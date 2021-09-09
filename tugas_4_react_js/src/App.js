05ee172dcd497a5332e04e85bd890ccbeb60d6cf
fajrulfalah17 <fajruljunior45@gmail.com>
2021-09-09 23:03:49 +0700

    Tugas 4 React Js
    Sudah siapp

---
+++ tugas_4_react_js/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ListMakanan from "./ListData/ListMakanan";
+import MenuMakanan from "./Page/MenuMakanan";
+function App() {
+  return (
+    <div className="App">
+    <MenuMakanan />
+    <ListMakanan />
+      
+    </div>
+  );
+}
+
+export default App;
